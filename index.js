0051c9e0d4d5d37aa462a47d53d5fa0ccf58fc51
Ayush Kumar <115471275+AyushKumar535@users.noreply.github.com>
2024-03-12 22:18:15 +0530

    Final Commits

---
+++ index.js
@@ -0,0 +1,20 @@
+function selectionSort() {
+    window.location.href = 'https://s21sd.github.io/SORT_VISUALIZER/SelectionSort.html'
+    // console.log("working");
+}
+function insertionSort() {
+    window.location.href = 'https://s21sd.github.io/SORT_VISUALIZER/InsertionSort.html'
+    // console.log("working");
+}
+function bubbleSort() {
+    window.location.href = 'https://s21sd.github.io/SORT_VISUALIZER/BubbleSort.html'
+    // console.log("working");
+}
+function quickSort() {
+    window.location.href = 'https://s21sd.github.io/SORT_VISUALIZER/QuickSort.html'
+    // console.log("working");
+}
+function mergeSort() {
+    window.location.href = 'https://s21sd.github.io/SORT_VISUALIZER/MergerSort.html'
+    // console.log("working");
+}
